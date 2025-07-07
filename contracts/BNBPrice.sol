3d69e4ef06831361598aedc48c3b1df12dc81d3f
tpointi-dev <tpointi.jcat@gmail.com>
2025-07-07 14:23:17 +0330

    IBNBPRICE

--- contracts/BNBPrice.sol
+++ contracts/BNBPrice.sol
@@ -5,6 +5,11 @@ import "@pancakeswap/v3-core/contracts/interfaces/IPancakeV3Factory.sol";
 import "@pancakeswap/v3-core/contracts/interfaces/IPancakeV3Pool.sol";
 import "@pancakeswap/v3-periphery/contracts/libraries/OracleLibrary.sol";
 
+// Main network bnb : 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c ( wraped )
+// Main network usd : 0x55d398326f99059fF775485246999027B3197955 ( Binance-Peg BSC-USD )
+// Main network factory : 0x0BFbCF9fa4f9C56B0F40a671Ad40E0805A091865
+// 100000000000000
+
 
 contract BNBPrice{
     address public bnb;
@@ -57,45 +62,7 @@ contract BNBPrice{
         
     }
     
-    function averageUsdToBnb() internal view returns (uint average) {
-        uint prices;
-        uint8 truePoolsLenght;
-        for (uint8 i = 0; i < poolsUSD.length ; i ++ )
-        {
-            (,int24 tick,,,,,) = IPancakeV3Pool(poolsUSD[i]).slot0();
-            uint price = OracleLibrary.getQuoteAtTick(
-                tick, 1e18, usd, bnb
-            );
-
-            if (price != 0) {
-                prices += price;
-                truePoolsLenght++;
-            }
-        }
-        require(prices != 0 , "No price founded");
-        average = prices/truePoolsLenght;
-        return average;
-    }
-
     
-    function averageBnbToUsd() internal view returns (uint average) {
-        uint prices;
-        uint8 truePoolsLenght;
-        for (uint8 i = 0; i < poolsUSD.length ; i ++ )
-        {
-            (,int24 tick,,,,,) = IPancakeV3Pool(poolsUSD[i]).slot0();
-            uint price = OracleLibrary.getQuoteAtTick(
-                tick, 1e18, bnb, usd
-            );
-            if (price != 0) {
-                prices += price;
-                truePoolsLenght++;
-            }
-        }
-        require(prices != 0 , "No price founded");
-        average = prices/truePoolsLenght;
-        return average;
-    }
     function _average( bool _mode ) internal view returns (uint average) {
         uint prices;
         uint8 truePoolsLenght;
@@ -105,7 +72,7 @@ contract BNBPrice{
             uint price = OracleLibrary.getQuoteAtTick(
                 tick, 
                 1e18, 
-                _mode ? usd : bnb ,
+                _mode ? usd : bnb , 
                 _mode ? bnb : usd
             );
             if (price != 0) {
@@ -122,36 +89,15 @@ contract BNBPrice{
         bnb = _bnb;
     }
 
-
-
     function setUsdAddress(address _usd) public onlyOwner{
         usd = _usd;
     }
 
-    receive() external payable {
-        // if(msg.value > 100000000000000){
-        //     if(eqMode[msg.sender]){
-        //         averageUsdToBnb();
-        //     } else {
-        //         averageBnbToUsd();
-        //     }
-        // } else {
-        //     returnZreo();
-        // }
-        msg.sender.transfer(msg.value);
-    }
 
 
     function getAverage() external payable returns (uint256 , string memory) {
-        require(msg.value >= 100000000000000, "Send more BNB");
-        // uint result = eqMode[msg.sender] ? averageUsdToBnb() : averageBnbToUsd() ;
+        require(msg.value == 100000000000000, "Send more BNB"); //=> 0.0001 bnb 
         uint result = _average(eqMode[msg.sender]);
-
-
-        // owner.transfer(msg.value);
-        // (bool success, ) = owner.call{value: msg.value}("");
-        // require(success, "Transfer failed");
-
         emit callAverage(msg.sender, msg.value);
 
         return (result , eqMode[msg.sender] ? "USDTBNB" : "BNBUSDT");
@@ -162,18 +108,29 @@ contract BNBPrice{
     function setEqMode() public {
         eqMode[msg.sender] = !eqMode[msg.sender];
     }
+
     function poolsLenght() public view returns(uint){
     uint count = poolsUSD.length;
     return count;
     }
 
     function getEqMode() public view returns( string memory) {
-        string memory em = eqMode[msg.sender] ? "BNB to USDT" : "USDT to BNB";
+        string memory em = eqMode[msg.sender] ?  "USDT to BNB" : "BNB to USDT";
         return em;
     }
 
     function changeOwner(address payable _newOwner) public onlyOwner {
         owner = _newOwner ; 
+    }
+
+    function getContractBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function toOwner() public onlyOwner {
+        require(address(this).balance >= 1000000000000000 , "LowBalance");
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success, "Transfer failed");
     }
 }
 
--- contracts/IBNBPrice.sol
+++ contracts/IBNBPrice.sol
@@ -2,7 +2,10 @@
 pragma solidity ^0.8.20;
 
 interface IBNBPrice {
-
-    function averageBnbToUsd() external view returns (uint);
-    function averageUsdToBnb() external view returns (uint);
+    
+    function setEqMode() external; 
+    function getEqMode() external view returns( string memory);
+    function getAverage() external payable returns (uint256 , string memory);
+    function bnbToUsd(uint8 index) external view  returns (uint);
+    function usdToBnb(uint8 index) external view  returns (uint);
 }
